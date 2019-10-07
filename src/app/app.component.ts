24e1170113935dad555d19fcbc32f59ea686c1ea
EvertonBorges <evertonsoaresborges@gmail.com>
2019-10-07 10:40:19 -0400

    Criando uma pagina de profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   public appPages = [
     {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
+      title: 'Profile',
+      url: '/profile',
+      icon: 'man'
+    },
+    {
+      title: 'Categorias',
+      url: '/categorias',
+      icon: 'bookmark'
     }
   ];
 

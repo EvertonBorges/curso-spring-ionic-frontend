44455679fe3194585c192340de4c6b27c58c1e2c
EvertonBorges <evertonsoaresborges@gmail.com>
2019-10-07 11:49:18 -0400

    Mostrando dados e imagem do cliente para pagina de profile

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { StorageService } from 'src/services/storage.service';
+import { ClienteDTO } from 'src/models/cliente.dto';
+import { ClienteService } from 'src/services/domain/cliente.service';
+import { API_CONFIG } from '../config/api.config';
 
 @Component({
   selector: 'app-profile',
@@ -8,9 +11,11 @@ import { StorageService } from 'src/services/storage.service';
 })
 export class ProfilePage implements OnInit {
 
-  email: string;
+  cliente: ClienteDTO;
 
-  constructor(public storage: StorageService) { }
+  constructor(
+    public storage: StorageService,
+    public clienteService: ClienteService) { }
 
   ngOnInit() {
   }
@@ -18,8 +23,28 @@ export class ProfilePage implements OnInit {
   ionViewDidEnter() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
-      this.email = localUser.email;
+      this.clienteService.findByEmail(localUser.email).subscribe(
+        response => {
+          this.cliente = response;
+          this.getImageIfExists();
+        },
+        error => {
+
+        }
+      )
     }
+  }
+
+  getImageIfExists() {
+    this.clienteService.getImageFromBucket(this.cliente.id)
+      .subscribe(
+        response => {
+          this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+        },
+        error => {
+
+        }
+      )
   }
 
 }

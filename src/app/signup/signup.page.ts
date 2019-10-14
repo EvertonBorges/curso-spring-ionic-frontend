f2f5c529a734499bda4a2d026d024c916da7cdc1
EvertonBorges <evertonsoaresborges@gmail.com>
2019-10-14 10:25:31 -0400

    Criando um formGroup para controlar o formulario

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  formGroup: FormGroup;
+
+  constructor(public formBuilder: FormBuilder) { 
+    this.formGroup = this.formBuilder.group({
+      nome: ['Joaquim', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+      email: ['joaguim@gmail.com', [Validators.required, Validators.email]],
+      tipo: ['1', [Validators.required]],
+      cpfOuCnpj: ['06134596280', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+      senha: ['123', [Validators.required]],
+      logradouro: ['Rua Via', [Validators.required]],
+      numero: ['25', [Validators.required]],
+      complemento: ['Apto 3', []],
+      bairro: ['Copabana', []],
+      cep: ['10828333', [Validators.required]],
+      telefone1: ['977261827', [Validators.required]],
+      telefone2: ['', []],
+      telefone3: ['', []],
+      estadoId: [null, [Validators.required]],
+      cidadeId: [null, [Validators.required]]
+    });
+  }
 
   ngOnInit() {
   }
